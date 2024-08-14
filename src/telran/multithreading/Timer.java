aba4ee0bc3f433e85a1e049d20f15e0b07ba8a14
Pavel Feldman <feldmanpk@gmail.com>
2024-08-14 16:38:42 +0300

    HW #45 Timer application ServiceThread

--- src/telran/multithreading/Timer.java
+++ src/telran/multithreading/Timer.java
@@ -1,21 +1,38 @@
 package telran.multithreading;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Timer extends Thread {
-	//TODO
 	//displaying time in a given format and a given resolution
-	//example displaying each second? or each 5 second, etc.
-	public Timer() {
+	//example displaying each second or each 5 second, etc.
+	private static final DateTimeFormatter DEFAULT_TIME_FORMATTER = DateTimeFormatter.ISO_LOCAL_TIME;
+	private static final Float DEFAULT_TIME_RESOLUTION = 1f;
+	DateTimeFormatter timeFormatter; 
+	Float timeResolution;
+	
+	public Timer(DateTimeFormatter timeFormatter, Float timeResolution) {
+		this.timeFormatter = timeFormatter;
+		this.timeResolution = timeResolution;
 		setDaemon(true);
 	}
+	public Timer(Float timeResolution) {
+		this(DEFAULT_TIME_FORMATTER, timeResolution);
+	}
+	public Timer(DateTimeFormatter timeFormatter) {
+		this(timeFormatter, DEFAULT_TIME_RESOLUTION);
+	}
+	public Timer() {
+		this(DEFAULT_TIME_FORMATTER, DEFAULT_TIME_RESOLUTION);
+	}
 	public void run() {
-		while(true) {
-			System.out.println(LocalTime.now());
+		boolean running = true;
+		while(running) {
+			System.out.println(LocalTime.now().format(timeFormatter));
 			try {
-				sleep(1000);
+				sleep((long) (timeResolution * 1000));
 			} catch (InterruptedException e) {
-
+				running = false;
 			}
 		}
 	}
--- src/telran/multithreading/TimerController.java
+++ src/telran/multithreading/TimerController.java
@@ -1,15 +1,45 @@
 package telran.multithreading;
 
+import java.time.format.DateTimeFormatter;
+
+import telran.view.InputOutput;
+import telran.view.SystemInputOutput;
+
 public class TimerController {
+	//figure out a solution allowing timer stop
+	//as example in 5 seconds you stop the timer
+	//following 5 seconds application is running with no timer
 
 	public static void main(String[] args) throws InterruptedException {
-		Timer timer = new Timer();
-		timer.start();
-		Thread.sleep(10_000);
-		//TODO
-		//figure out a solution allowing timer stop
-		//as example in 5 seconds you stop the timer
-		//following 5 seconds application is running with no timer
+		InputOutput io = new SystemInputOutput();
+		System.out.println("***** Timer application *****\n");
+		String timePattern = io.readString(
+				"Enter the time format using pattern letters & delimeters (e.g. \"HH-mm-ss.S a\") or press \"Enter\" for the default format:");
+		String timeResolution = io.readString(
+				"Enter the time resolution in seconds (integer or float number) or press \"Enter\" for the default value:");
+		
+		try {
+			Timer timer = switch ((timePattern.isEmpty() ? "0" : "*") + ":" + (timeResolution.isEmpty() ? "0" : "*")) {
+				case "0:0" -> new Timer();
+				case "0:*" -> new Timer(Float.parseFloat(timeResolution));
+				case "*:0" -> new Timer(DateTimeFormatter.ofPattern(timePattern));
+				default -> new Timer(DateTimeFormatter.ofPattern(timePattern), Float.parseFloat(timeResolution));
+			};
+			System.out.println("Press \"Enter\" button to stop the timer");
+			timer.start();
+			
+			Thread stoppingThread = new Thread (() -> {
+				io.readString("");
+				timer.interrupt();
+				System.out.println("Timer stopped");
+			});
+			stoppingThread.setDaemon(true);
+			stoppingThread.start();
+			Thread.sleep(10_000);
+		} catch (RuntimeException e) {
+			System.out.println("Input data is incorrect");
+		}
+		System.out.println("Application finished");
 
 	}
 
